5c83ef3165df0f13d0718c304e4343bacdbafc50
Ubuntu <ubuntu@ip-10-196-218-101.ec2.internal>
2012-01-08 01:38:52 +0000

    first code

--- namespace.js
+++ namespace.js
@@ -1,4 +1,20 @@
 /**
  * The code
+ * author: Fabio Issamu Oshiro (Sr. Oshiro)
  */
-
+function namespace(name, publics){
+    var path = name.split('\.');
+    var cpath = 'this.' + path[0];
+    for(var i=0;i<path.length;i++){
+        if(typeof(eval('this.' + cpath)) == 'undefined'){
+            eval(cpath + '={}');
+        }
+        cpath += '.' + path[i];
+    }
+    var ns = eval(nome);
+    var key;
+    if(typeof(publics) == 'function') publics = publics();
+    for(key in publics){
+        ns[key] = publics[key];
+    }
+}
