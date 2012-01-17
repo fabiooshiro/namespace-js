c5bbda7b0ee34f4459efb423a3d47e7e452942a2
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2012-01-16 23:39:37 -0200

    WTFPL - Do What The Fuck You Want To Public License

--- namespace.js
+++ namespace.js
@@ -1,6 +1,13 @@
 /**
- * The source code
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ * 
+ * The source code is at
  * https://github.com/fabiooshiro/namespace-js
+ * 
  * author: Fabio Issamu Oshiro (Sr. Oshiro)
  * ref: http://blog.stannard.net.au/2011/01/14/creating-namespaces-in-javascript/
  */
